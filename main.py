7599a12ac29a5cbe14c48e14044dd95f6e67de75
miyamotojunpei <miyamoto.j.aa@m.titech.ac.jp>
2019-05-03 21:58:55 +0900

    [update] display only tweet['text']

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def delete_gbf_tweets(tweets, session):
 def delete_auto_tweets(result, session):
     for tweets in result:
         for tweet in tweets['statuses']:
-            print(tweet)
+            print(tweet['text'])
             delete_tweet(tweet, session)
 
 
